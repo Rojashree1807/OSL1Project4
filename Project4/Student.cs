b54bf5e9fbb5f9733117e69ea8319ad583045551
Rojashree1807 <rojarojashree1807@gmail.com>
2023-12-05 11:49:57 +0530

    Initial commit

---
+++ Project4/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    public class Student
+    {
+        string name, section;
+        public string Name { get { return name; } set { name = value; } }
+        public string Section { get { return section; } set { section = value; } }
+        public Student(string name, string section)
+        {
+            this.Name = name;
+            this.Section = section;
+        }
+    }
+}
